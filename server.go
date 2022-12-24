9f158f307552195ee9888a8cf99d825be27b0b53
dh0ck <34309036+dh0ck@users.noreply.github.com>
2022-12-24 12:26:05 +0100

    Add files via upload

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+      http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello!")
+    })
+
+    fmt.Printf("Starting server at port 8081\n")
+        if err := http.ListenAndServe(":8081", nil); err != nil {
+        log.Fatal(err)
+    }
+}

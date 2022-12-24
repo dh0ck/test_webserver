a772803e744dea50e0e59d6681bda5774a1eda67
dh0ck <34309036+dh0ck@users.noreply.github.com>
2022-12-24 16:20:24 +0100

    Update server.go

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
       http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello! test")
+        fmt.Fprintf(w, "Hello! test 1)
     })
 
     fmt.Printf("Starting server at port 8081\n")
